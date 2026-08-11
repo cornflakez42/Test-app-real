ca6a2c1ce0595cc3cff9127f49097c661a243304
cornflakez42 <charllesmcadam@gmail.com>
2026-08-11 22:25:27 +0100

    Update age.py

--- age.py
+++ age.py
@@ -6,7 +6,7 @@ YELLOW = "\033[33m"
 CYAN = "\033[36m"
 RED = "\033[31m"
 
-# function  age calc
+# function age calc
 def about():
     #gets todays date
     today = date.today()
@@ -20,7 +20,7 @@ def about():
     while True:
         try:
             dobd = int(input(YELLOW+"please enter the day u were born eg 07 "+CYAN))
-            if 1 <= dobd <= 31: # Optional check to make sure day is a real calendar number
+            if 1 <= dobd <= 31:
                 break
             else:
                 print(RED + "Invalid day! Please enter a number between 1 and 31." + CYAN)
@@ -28,11 +28,10 @@ def about():
             print(RED + "Error: Please enter numbers only, no letters!" + CYAN)
             print("")
              
-    #same as line 11
     while True:
         try:
             dobm = int(input(YELLOW+"please enter the month u were born eg 04 "+CYAN))
-            if 1 <= dobm <= 12: # Optional check to make sure month is a real calendar number
+            if 1 <= dobm <= 12:
                 break
             else:
                 print(RED + "Invalid month! Please enter a number between 1 and 12." + CYAN)
@@ -47,7 +46,7 @@ def about():
     while True:
         try:
             dobyear = int(input(YELLOW+"Please enter the year you were born eg 2003 "+CYAN))
-            if 1900 <= dobyear <= today.year: # Makes sure they don't type a future year
+            if 1900 <= dobyear <= today.year:
                 break
             else:
                 print(RED + f"Invalid year! Please enter a year between 1900 and {today.year}." + CYAN)
@@ -58,11 +57,9 @@ def about():
     #calculates current age by subtracting current year
     age = today.year - dobyear
     
-    # corrected calculation bounds check logic for birthdays
     if dobm > today.month or (dobm == today.month and dobd > today.day):
         age -= 1
 
-    #puts it all together and print the tezt back with the stored variables
     print(f"{YELLOW}your name is {name.capitalize()} your age is {age} you were born {dobd}/{dobm}/{dobyear}")
     print()
     input(YELLOW + "Press Enter to return to menu..." + CYAN)
